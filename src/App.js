234b97e65fdb58d666a2f75dbf9889767f85f59b
Satyaki Bose <satyakibose98@gmail.com>
2021-02-01 21:00:06 +0530

    First commmit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Values from "values.js";
 
+import values from "values.js";
 function App() {
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      console.log(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="container">
+        <h3>Color Generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            placeholder="#f15025"
+            onChange={(e) => setColor(e.target.value)}
+          />
+          <button className="btn">submit</button>
+        </form>
+      </section>
+      <section className="colors">
+        <h4>List goes here</h4>
+      </section>
+    </>
   );
 }
 
