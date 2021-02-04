8cfd22d412f50987492485662ee3c69d0f547409
Satyaki Bose <satyakibose98@gmail.com>
2021-02-04 16:31:20 +0530

    Add Copy Clipboard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Values from "values.js";
-
-import values from "values.js";
+import SingleColor from "./SingleColor";
 function App() {
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
@@ -11,7 +10,7 @@ function App() {
     e.preventDefault();
     try {
       let colors = new Values(color).all(10);
-      console.log(colors);
+      setList(colors);
     } catch (error) {
       setError(true);
       console.log(error);
@@ -33,7 +32,16 @@ function App() {
         </form>
       </section>
       <section className="colors">
-        <h4>List goes here</h4>
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
       </section>
     </>
   );
---
+++ src/SingleColor.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./utils";
+
+const SingleColor = ({ rgb, weight, hexColor, index }) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(",");
+  // const hex = rgbToHex(...rgb);  // this can also be used to display the hex code of the colors
+  const hexValue = `#${hexColor}`;
+
+  const handleColorClick = () => {
+    setAlert(true);
+    navigator.clipboard.writeText(hexValue);
+  };
+  return (
+    <article
+      className={`color ${index > 10 && "color-light"}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={handleColorClick}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert">Copied to clipboard</p>}
+    </article>
+  );
+};
+
+export default SingleColor;
